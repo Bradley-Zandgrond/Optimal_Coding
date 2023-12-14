ef1ce9a65c67bea1f7aff2e92cac627d98780a76
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-12-14 08:55:19 +0100

    added extra

---
+++ project2_Extra/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voorbeeld2 menu project</title>
+</head>
+<body>
+    <h1>Voorbeeld2 menu project</h1>
+    <nav>
+        <ul>
+            <li><a href="page1.php">page 1</a></li>
+        </ul>
+    </nav>    
+</body>
+</html>
